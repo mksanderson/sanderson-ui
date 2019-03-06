cbdcb976f0456451f3c55283d45b24cf9f24f90c
mksanderson <matthew@sanderson.id.au>
2019-03-06 21:01:55 +1000

    Fixed an issue with the page frame background color

--- projects/sanderson-ui/src/pages/page/page-frame/page-frame.component.ts
+++ projects/sanderson-ui/src/pages/page/page-frame/page-frame.component.ts
@@ -1,10 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, ContentChildren, HostBinding} from '@angular/core';
+import {LinkagesComponent} from "../../../linkages/linkages.component";
 
 @Component({
   selector: 'page-frame',
-  styleUrls: ['./page-frame.component.scss'],
+  styleUrls: [
+    './page-frame.component.scss',
+    './page-frame.theme.scss'
+  ],
   templateUrl: './page-frame.component.html'
 })
 export class PageFrameComponent {
-
+  @ContentChildren(LinkagesComponent) @HostBinding('class.linkages') public linkages: LinkagesComponent;
 }
--- projects/sanderson-ui/src/pages/page/page-view/page-view.component.ts
+++ projects/sanderson-ui/src/pages/page/page-view/page-view.component.ts
@@ -1,9 +1,12 @@
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
 
 @Component({
-  selector: "page-view",
-  styleUrls: ["./page-view.component.scss"],
-  templateUrl: "./page-view.component.html"
+  selector: 'page-view',
+  styleUrls: [
+    './page-view.component.scss',
+    './page-view.theme.scss'
+  ],
+  templateUrl: './page-view.component.html'
 })
 export class PageViewComponent {
 
